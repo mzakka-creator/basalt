b5dab3a4ae5c43017d086fe96ad5aa8503068d5a
Michael <a.afif@tgmena.com>
2026-07-12 14:10:57 +0300

    video

--- lib/factory-video.ts
+++ lib/factory-video.ts
@@ -1,17 +1,5 @@
-export const FACTORY_VIDEO_DRIVE_ID = '1LhfB5Hnd9wbujTLpfWOSDTxkny8HutMw';
-
-export const FACTORY_VIDEO_LOCAL_SRC = '/factory/factory-tour.mp4';
-
-export const FACTORY_VIDEO_API_SRC = '/api/factory-video';
+export const FACTORY_VIDEO_SRC = '/video/basalt-video.mp4';
 
 export function getFactoryVideoSrc(): string {
-  if (process.env.NEXT_PUBLIC_FACTORY_VIDEO_URL) {
-    return process.env.NEXT_PUBLIC_FACTORY_VIDEO_URL;
-  }
-
-  if (process.env.NODE_ENV === 'development') {
-    return FACTORY_VIDEO_LOCAL_SRC;
-  }
-
-  return FACTORY_VIDEO_API_SRC;
+  return process.env.NEXT_PUBLIC_FACTORY_VIDEO_URL ?? FACTORY_VIDEO_SRC;
 }
